a30822f0d492818a844661f74feb38325ec415ec
kevinmorrisnet <kevinmorrisone@icloud.com>
2026-08-08 19:55:09 -0400

    dumb

--- eslint.config.js
+++ eslint.config.js
@@ -5,6 +5,7 @@ import pluginReact from "eslint-plugin-react";
 export default [
   js.configs.recommended,
   pluginReact.configs.flat.recommended,
+  pluginReact.configs.flat["jsx-runtime"],
   {
     files: ["**/*.jsx"],
     settings: {
@@ -14,6 +15,12 @@ export default [
     },
     languageOptions: {
       globals: globals.browser,
+    },
+  },
+  {
+    files: ["**/*.test.jsx"],
+    languageOptions: {
+      globals: globals.vitest,
     },
   },
 ];
